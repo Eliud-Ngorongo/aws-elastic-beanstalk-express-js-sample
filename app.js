e8265aaa737c75eae4f302ecd0bf56ed4da9cbf2
Eliud-Ngorongo <eliudnjenga@gmail.com>
2023-03-16 12:57:10 +0300

    Checking if review stage works

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!, Now running on AWS. And after this we have succesfully built a pipeline!'));
+app.get('/', (req, res) => res.send('Hello World!, I have succesfully built a pipeline on AWS!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
